a5fe5ac424436b00471c074b557f6d344a42276a
EMRAgit <49976179+EMRAgit@users.noreply.github.com>
2019-04-25 10:43:22 +0200

    Create fMODIS.R

---
+++ fMODIS.R
@@ -0,0 +1,8 @@
+#The R package is stored and documented on github (https://github.com/cran/gWidgetsRGtk2.git)
+#Installation of gWidgetsRGtk2 on R
+install.packages("gWidgetsRGtk2")
+#Call libraries
+library(gWidgetsRGtk2)
+library("MODIStsp", lib.loc="~/R/win-library/3.5")
+#Call GUI
+MODIStsp()
